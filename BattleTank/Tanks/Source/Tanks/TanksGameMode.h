a512513ccb07d0cc26fd27435c895e9101b5aa3c
zecbmo <gmccartan05@gmail.com>
2016-10-25 22:27:58 +0100

    ProjectStarted

---
+++ BattleTank/Tanks/Source/Tanks/TanksGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TanksGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKS_API ATanksGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
